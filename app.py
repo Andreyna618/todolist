af3d51b61acb5d92f1a6879542b145fd1b7839f7
Andreyna618 <andreynagb@hotmail.com>
2022-11-01 13:16:12 +0000

    flask instalado

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app=Flask(__name__)
+@app.route('/')
+
+def home():
+    return('Hello,web!')
+
+@app.route('/bye')   
+def bye() :
+    return 'bye'
+app.run(debug=True)
